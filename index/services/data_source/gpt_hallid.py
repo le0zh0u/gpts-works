a272127a373677576cc0a135cf8c708fe1e7fc91
leozhou <492753662@qq.com>
2023-11-20 14:47:14 +0800

    add gpt_hallid

--- index/services/data_source/gpt_hallid.py
+++ index/services/data_source/gpt_hallid.py
@@ -1,2 +1,12 @@
 # data source: https://gpts.hallid.ai/
 
+import requests
+import json
+
+
+def fetch_hallid_json():
+    url = ""
+    response = requests.get(url)
+    response.raise_for_status()
+
+    
